2fb6faca36a98841cc606435a595684de5bf0db9
Duke Honety <mitchellcaleb00@gmail.com>
2022-10-15 18:18:03 +0900

    feat: integrate `auth`

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::namespace('Auth')->group(function () {
+  Route::get('/login','LoginController@show_login_form')->name('login');
+  Route::post('/login','LoginController@process_login')->name('login');
+  Route::get('/register','LoginController@show_signup_form')->name('register');
+  Route::post('/register','LoginController@process_signup');
+  Route::post('/logout','LoginController@logout')->name('logout');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
